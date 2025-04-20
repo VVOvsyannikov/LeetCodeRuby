3fd31449a68f75a96583e7f7cd6764a77a16b6ff
msk-hq-nb-2333 <vovsyannikov@samokat.ru>
2025-04-20 23:22:37 +0300

    1971. Find if Path Exists in Graph

---
+++ graph/1971_find_if_path_exists_in_graph.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+def valid_path(n, edges, source, destination)
+  if edges.nil? || source.nil? || destination.nil? || source >= n || source.negative? || destination >= n || destination.negative?
+    return false
+  end
+  return true if source == destination
+
+  graph = Hash.new { |h, k| h[k] = [] }
+
+  edges.each do |a, b|
+    graph[a] << b
+    graph[b] << a
+  end
+
+  stack = [source]
+  visited = Set.new
+
+  while stack.size.positive?
+    vertex = stack.pop
+
+    graph[vertex].each do |neighbour|
+      if neighbour && !visited.include?(neighbour)
+        visited.add(neighbour)
+        stack << neighbour
+      end
+    end
+  end
+
+  visited.include?(destination)
+end
