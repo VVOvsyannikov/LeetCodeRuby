5340c2ea531ce708a6b71fe0ba2a5c9e513d1fbf
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2025-05-15 11:27:50 +0300

    46. Permutations

---
+++ backtracking/46_permutations.rb
@@ -0,0 +1,20 @@
+def permute(nums)
+  backtrack([], nums, [])
+end
+
+def backtrack(acc, nums, result)
+  if acc.size == nums.size
+    result << acc.dup
+    return nil
+  end
+
+  nums.each do |num|
+    next if acc.include?(num)
+
+    acc << num
+    backtrack(acc, nums, result)
+    acc.pop
+  end
+
+  result
+end
